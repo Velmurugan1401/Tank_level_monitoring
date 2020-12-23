3407fa9a55a91b81fd81f7f2185b99c292662541
velmurugan1401 <velvel1401@gmail.com>
2020-12-23 13:29:29 -0800

    update

--- modules/tables.js
+++ modules/tables.js
@@ -1,11 +1,11 @@
 
 var Tables = function (app) {
 
-    this.TANK_TABLE = 2001;
-    this.USER_TABLE = 2001;
-    this.TANKHISTORY_TABLE = 2001;
-    this.TANKSTATUS_TABLE = 2001;
-    this.DEVICE_TABLE = 2001;
+    this.TANK_TABLE=1002;
+    this.USER_TABLE = 1001;
+    this.TANKHISTORY_TABLE = 1004;
+    this.TANKSTATUS_TABLE = 1003;
+    this.DEVICE_TABLE = 1005;
 
 
 };
