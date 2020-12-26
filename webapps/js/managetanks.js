717a3f271f743a87de4399ff88a05e4a52a1ccfd
mradhika12 <rathi2000ka@gmail.com>
2020-12-26 11:15:14 +0530

    pull commit

--- webapps/js/managetanks.js
+++ webapps/js/managetanks.js
@@ -1,9 +1,19 @@
+// var StudentTable = null;
+// var student_list = [];
 
+// $(document).ready(function(){
+//     loadStudentList();
+// });
+// function loadStudentList() {
 
+//     if (StudentTable) {
+//         StudentTable.destroy();
+//         $("#example").html("");
+//     }
 //  var fields = [
 //         {
 //             mData: 'tankname',
-//             Title: 'Tank Name',
+//             sTitle: 'Tank Name',
 //             sWidth: '20%',
 //             orderable: false,
 //             mRender: function (data, type, row) {
@@ -12,7 +22,7 @@
 //         },
 //         // {
 //         //     mData: 'location',
-//         //     Title: 'Location',
+//         //     sTitle: 'Location',
 //         //     sWidth: '20%',
 //         //     orderable: false,
 //         //     mRender: function (data, type, row) {
@@ -23,7 +33,7 @@
 //         {
 //             mData: 'tanktype',
 //             sWidth: '20%',
-//             Title: 'Tank Type',
+//             sTitle: 'Tank Type',
 //             orderable: false,
 //             mRender: function (data, type, row) {
 //                 return large;
@@ -32,7 +42,7 @@
 //         {
 //             mData: 'capacity',
 //             sWidth: '20%',
-//             Title: 'Capacity',
+//             sTitle: 'Capacity',
 //             orderable: false,
 //             mRender: function (data, type, row) {
 //                 return 1112;
@@ -40,7 +50,7 @@
 //         },
 //         {
 //             mData: 'linked_devices',
-//             Title: 'Linked Devices',
+//             sTitle: 'Linked Devices',
 //             "className": 'sortingtable',
 //             orderable: false,
 //             mRender: function (data, type, row) {
@@ -49,7 +59,7 @@
 //         },
 //         {
 //             mData: 'created_ts',
-//             Title: 'Created Time',
+//             sTitle: 'Created Time',
 //             "className": 'sortingtable',
 //             mRender: function (data, type, row) {
 //                 return moment(data).format(DATE_TIME_FORMAT);
@@ -57,14 +67,14 @@
 //         },
 //         {
 //             mData: 'updated_ts',
-//             Title: 'Updated Time',
+//             sTitle: 'Updated Time',
 //             "className": 'sortingtable',
 //             mRender: function (data, type, row) {
 //                 return moment(data).format(DATE_TIME_FORMAT);
 //             }
 //         },
 //         {
-//             Title: 'Actions',
+//             sTitle: 'Actions',
 //             orderable: false,
 //             mRender: function (data, type, row) {
 //                 var actionsHtml = '<button class="btn btn-default" onclick="deleteStudent(\'' + row._id + '\')"><i class="fa fa-trash"></i></button> <button class="btn btn-default" onclick="editStudent(\'' + row._id + '\')"><i class="fa fa-pencil" aria-hidden="true"></i></button>';
@@ -74,6 +84,102 @@
 //         }
     
 //     ];
+
+// var queryParams = {
+//     query: {
+//         "bool": {
+//             "must": []
+//         }
+//     },
+//     sort: [{ "created_ts": { "order": "asc" } }]
+// };
+
+// student_list = [];
+
+// var tableOption = {
+//     fixedHeader: false,
+//     responsive: false,
+//     paging: true,
+//     searching: true,
+//     aaSorting: [[3, 'desc']],
+//     "ordering": true,
+//     iDisplayLength: 10,
+//     lengthMenu: [[10, 50, 100], [10, 50, 100]],
+//     aoColumns: fields,
+//     "bProcessing": true,
+//     "language": {
+//         "emptyTable": "No data found!",
+//         "processing": '<i class="fa fa-spinner fa-spin" style="color:#333"></i> Processing'
+
+//     },
+//     "bServerSide": true,
+//     "sAjaxSource": BASE_PATH+'/student/list',
+//     "fnServerData": function (sSource, aoData, fnCallback, oSettings) {
+
+
+//         queryParams.query['bool']['must'] = [];
+//         queryParams.query['bool']['should'] = [];
+//         delete queryParams.query['bool']["minimum_should_match"];
+
+//         var keyName = fields[oSettings.aaSorting[0][0]]
+
+//         var sortingJson = {};
+//         sortingJson[keyName['mData']] = { "order": oSettings.aaSorting[0][1] };
+//         queryParams.sort = [sortingJson];
+
+//         queryParams['size'] = oSettings._iDisplayLength;
+//         queryParams['from'] = oSettings._iDisplayStart;
+
+//         // queryParams.query['bool']['must'].push({ "match": { "acc_id":SESSION_OBJ.orgs[0]  } });
+
+//         var searchText = oSettings.oPreviousSearch.sSearch.trim();
+
+//         if (searchText) {
+//             queryParams.query['bool']['should'].push({ "wildcard": { "sname": "*" + searchText + "*" } });
+//             queryParams.query['bool']['should'].push({ "wildcard": { "sname": "*" + searchText.toLowerCase() + "*" } });
+//             queryParams.query['bool']['should'].push({ "wildcard": { "sname": "*" + searchText.toUpperCase() + "*" } });
+//             queryParams.query['bool']['should'].push({ "wildcard": { "sname": "*" + capitalizeFLetter(searchText) + "*" } })
+//             queryParams.query['bool']["minimum_should_match"] = 1;
+//             queryParams.query['bool']['should'].push({
+//                 "match_phrase": {
+//                     "sname.keyword": "*" + searchText + "*"
+//                 }
+//             })
+//             queryParams.query['bool']['should'].push({
+//                 "match_phrase_prefix": {
+//                     "sname.keyword": {
+//                         "query": "*" + searchText + "*"
+//                     }
+//                 }
+//             });
+//         }
+
+//         oSettings.jqXHR = $.ajax({
+//             "dataType": 'json',
+//             "contentType": 'application/json',
+//             "type": "POST",
+//             "url": sSource,
+//             "data": JSON.stringify({"query":queryParams}),
+//             success: function (data) {
+
+//                 var resultData = data.result.data;
+
+//                 student_list = resultData.data;
+
+//                 $(".totalCount").html(data.result.total)
+
+//                 resultData['draw'] = oSettings.iDraw;
+//                 fnCallback(resultData);
+//             }
+//         });
+//     },
+//     "initComplete": function (settings, json) {
+//     }
+// };
+
+// StudentTable = $("#example").DataTable(tableOption);
+// }
+
 var fileds=[
     [ "DWF", "large", "12220", "ggg", "2011/04/25", "2020/09/08","delete" ],
     [ "Garrett Winters", "Accountant", "Tokyo", "8422", "2011/07/25", "$170,750","kkk" ],
@@ -99,3 +205,4 @@ var fileds=[
             ]
         } );
     } );
+
