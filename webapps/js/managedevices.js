a629b94f29833d5493082217508e1694a70f6cd2
kailashsolai10 <kailashsolai10@gmail.com>
2020-12-26 14:57:03 +0530

    pull commit

--- webapps/js/managedevices.js
+++ webapps/js/managedevices.js
@@ -1,337 +1,105 @@
 
-//             var fields = [
-//                 {
-//                     mData: 'sname',
-//                     sTitle: 'Device Id',
-//                     sWidth: '20%',
-//                     orderable: false,
-//                     mRender: function (data, type, row) {
-//                         return data;
-//                     }
-//                 },
-//                 {
-//                     mData: 'sname',
-//                     sTitle: 'Device Model',
-//                     sWidth: '20%',
-//                     orderable: false,
-//                     mRender: function (data, type, row) {
-//                         return data;
-//                     }
-//                 },
-//                 {
-//                     mData: 'sname',
-//                     sTitle: 'Version',
-//                     sWidth: '20%',
-//                     orderable: false,
-//                     mRender: function (data, type, row) {
-//                         return data;
-//                     }
-//                 },
-//                 {
-//                     mData: 'location',
-//                     sTitle: 'Channel',
-//                     sWidth: '20%',
-//                     orderable: false,
-//                     mRender: function (data, type, row) {
-//                         return data;
-//                     }
-//                 },
+            // var fields = [
+            //     {
+            //         mData: 'sname',
+            //         sTitle: 'Device Id',
+            //         sWidth: '20%',
+            //         orderable: false,
+            //         mRender: function (data, type, row) {
+            //             return data;
+            //         }
+            //     },
+            //     {
+            //         mData: 'sname',
+            //         sTitle: 'Device Model',
+            //         sWidth: '20%',
+            //         orderable: false,
+            //         mRender: function (data, type, row) {
+            //             return data;
+            //         }
+            //     },
+            //     {
+            //         mData: 'sname',
+            //         sTitle: 'Version',
+            //         sWidth: '20%',
+            //         orderable: false,
+            //         mRender: function (data, type, row) {
+            //             return data;
+            //         }
+            //     },
+            //     {
+            //         mData: 'location',
+            //         sTitle: 'Channel',
+            //         sWidth: '20%',
+            //         orderable: false,
+            //         mRender: function (data, type, row) {
+            //             return data;
+            //         }
+            //     },
         
-//                 {
-//                     mData: 'department',
-//                     sWidth: '20%',
-//                     sTitle: 'Status',
-//                     orderable: false,
-//                     mRender: function (data, type, row) {
-//                         return data;
-//                     }
-//                 },
-//                 {
-//                     mData: 'created_ts',
-//                     sTitle: 'Last Reported Time',
-//                     "className": 'sortingtable',
-//                     mRender: function (data, type, row) {
-//                         return moment(data).format(DATE_TIME_FORMAT);
-//                     }
-//                 },
-//                 {
-//                     mData: 'created_ts',
-//                     sTitle: 'Created Time',
-//                     "className": 'sortingtable',
-//                     mRender: function (data, type, row) {
-//                         return moment(data).format(DATE_TIME_FORMAT);
-//                     }
-//                 },
-//                 {
-//                     sTitle: 'Action',
-//                     orderable: false,
-//                     mRender: function (data, type, row) {
-//                         var actionsHtml = '<button class="btn btn-default" onclick="deleteStudent(\'' + row._id + '\')"><i class="fa fa-trash"></i></button> <button class="btn btn-default" onclick="editStudent(\'' + row._id + '\')"><i class="fa fa-pencil" aria-hidden="true"></i></button>';
-//                         return actionsHtml;
+            //     {
+            //         mData: 'department',
+            //         sWidth: '20%',
+            //         sTitle: 'Status',
+            //         orderable: false,
+            //         mRender: function (data, type, row) {
+            //             return data;
+            //         }
+            //     },
+            //     {
+            //         mData: 'created_ts',
+            //         sTitle: 'Last Reported Time',
+            //         "className": 'sortingtable',
+            //         mRender: function (data, type, row) {
+            //             return moment(data).format(DATE_TIME_FORMAT);
+            //         }
+            //     },
+            //     {
+            //         mData: 'created_ts',
+            //         sTitle: 'Created Time',
+            //         "className": 'sortingtable',
+            //         mRender: function (data, type, row) {
+            //             return moment(data).format(DATE_TIME_FORMAT);
+            //         }
+            //     },
+            //     {
+            //         sTitle: 'Action',
+            //         orderable: false,
+            //         mRender: function (data, type, row) {
+            //             var actionsHtml = '<button class="btn btn-default" onclick="deleteStudent(\'' + row._id + '\')"><i class="fa fa-trash"></i></button> <button class="btn btn-default" onclick="editStudent(\'' + row._id + '\')"><i class="fa fa-pencil" aria-hidden="true"></i></button>';
+            //             return actionsHtml;
                         
-//                     }
-//                 }
+            //         }
+            //     }
+// ];
+            var fields = [
+                ["aa","bb","cc","dd","aa","bb","cc","dd"],
+           ["aa","bb","cc","dd","aa","bb","cc","dd"],
+           ["aa","bb","cc","dd","aa","bb","cc","dd"],
+           ["aa","bb","cc","dd","aa","bb","cc","dd"]
+            ];
+
+$(document).ready(function() {
+    $('#manage-device').DataTable( {
+        
+            data: fields,
+        columns: [
+            { title: "Device Id" },
+            { title: "Device Model" },
+            { title: "Version" },
+            { title: "Channel" },
+            { title: "Status" },
+            { title: "Last Reported Time" },
+            { title: "Created Time" },
+            { title: "Action" },
             
-//             ];
-//             //[["aa","bb","cc","dd","aa","bb","cc","dd"],
-// //             ["aa","bb","cc","dd","aa","bb","cc","dd"],
-// //             ["aa","bb","cc","dd","aa","bb","cc","dd"],
-// //             ["aa","bb","cc","dd","aa","bb","cc","dd"]
-    
-
-// // ];
-// $(document).ready(function() {
-//     $('#manage-device').DataTable( {
-//         data: fields,
-//         columns: [
-//             { title: "Device Id" },
-//             { title: "Device Model" },
-//             { title: "Version" },
-//             { title: "Channel" },
-//             { title: "Status" },
-//             { title: "Last Reported Time" },
-//             { title: "Created Time" },
-//             { title: "Action" },
-            
-//         ]
-//     } );
-// } );
-
-
-var StudentTable = null;
-var student_list = [];
-// var startDate = moment().subtract(6, 'days').startOf('day');
-// var endDate = moment().endOf('day');
-$(document).ready(function(){
-    loadStudentList();
-});
-
-//Student Registration API
-function studentRegistration(){
-
-    var sname = $("#sname").val();
-    var department = $("#department").val();
-    var location = $("#location").val();
-
-    //Validate
-    if(sname === ""){
-
-        alert("Student Name is Required!");
-
-    }else if(department === ""){
-
-        alert("Department is Required!");
-
-    }else if(location === ""){
-
-        alert("Location is Required!");
-
-    }else{
-
-        //Build Input Objects
-        var inputObj = {
-            sname : sname,
-            location : location,
-            department : department,
-            created_ts : new Date().getTime()
-        };
-
-        //Call API
-        $.ajax({
-            url: BASE_PATH+"/student/insert",
-            data: JSON.stringify(inputObj),
-            contentType: "application/json",
-            type: 'POST',
-            success: function (result) {
-
-                //Success -> Show Alert & Refresh the page
-                successMsg("Registration Completed Successfully!");
-                loadStudentList();
-            },
-            error: function (e) {
-
-                //Error -> Show Error Alert & Reset the form
-                errorMsg("Registration Failed!");
-                window.location.reload();
-            }
-        });
-    }
-}
-
-//Student List API
-function loadStudentList() {
-
-    if (StudentTable) {
-        StudentTable.destroy();
-        $("#student_table").html("");
-    }
-
-    var fields = [
-        {
-                                mData: 'sname',
-                                sTitle: 'Device Id',
-                                sWidth: '20%',
-                                orderable: false,
-                                mRender: function (data, type, row) {
-                                    return data;
-                                }
-                            },
-                            {
-                                mData: 'sname',
-                                sTitle: 'Device Model',
-                                sWidth: '20%',
-                                orderable: false,
-                                mRender: function (data, type, row) {
-                                    return data;
-                                }
-                            },
-                            {
-                                mData: 'sname',
-                                sTitle: 'Version',
-                                sWidth: '20%',
-                                orderable: false,
-                                mRender: function (data, type, row) {
-                                    return data;
-                                }
-                            },
-                            {
-                                mData: 'location',
-                                sTitle: 'Channel',
-                                sWidth: '20%',
-                                orderable: false,
-                                mRender: function (data, type, row) {
-                                    return data;
-                                }
-                            },
-                    
-                            {
-                                mData: 'department',
-                                sWidth: '20%',
-                                sTitle: 'Status',
-                                orderable: false,
-                                mRender: function (data, type, row) {
-                                    return data;
-                                }
-                            },
-                            {
-                                mData: 'created_ts',
-                                sTitle: 'Last Reported Time',
-                                "className": 'sortingtable',
-                                mRender: function (data, type, row) {
-                                    return moment(data).format(DATE_TIME_FORMAT);
-                                }
-                            },
-                            {
-                                mData: 'created_ts',
-                                sTitle: 'Created Time',
-                                "className": 'sortingtable',
-                                mRender: function (data, type, row) {
-                                    return moment(data).format(DATE_TIME_FORMAT);
-                                }
-                            },
-                            {
-                                sTitle: 'Action',
-                                orderable: false,
-                                mRender: function (data, type, row) {
-                                    var actionsHtml = '<button class="btn btn-default" onclick="deleteStudent(\'' + row._id + '\')"><i class="fa fa-trash"></i></button> <button class="btn btn-default" onclick="editStudent(\'' + row._id + '\')"><i class="fa fa-pencil" aria-hidden="true"></i></button>';
-                                    return actionsHtml;
-                                    
-                                }
-                            }
-    ];
-
-    var queryParams = {
-        query: {
-            "bool": {
-                "must": []
-            }
-        },
-        sort: [{ "created_ts": { "order": "asc" } }]
-    };
-
-    student_list = [];
-
-    var tableOption = {
-        fixedHeader: false,
-        responsive: false,
-        paging: true,
-        searching: true,
-        aaSorting: [[3, 'desc']],
-        "ordering": true,
-        iDisplayLength: 10,
-        lengthMenu: [[10, 50, 100], [10, 50, 100]],
-        aoColumns: fields,
-        "bProcessing": true,
-        "language": {
-            "emptyTable": "No data found!",
-            "processing": '<i class="fa fa-spinner fa-spin" style="color:#333"></i> Processing'
-
-        },
-        "bServerSide": true,
-        "sAjaxSource": BASE_PATH+'/student/list',
-        "fnServerData": function (sSource, aoData, fnCallback, oSettings) {
-
-
-            queryParams.query['bool']['must'] = [];
-            queryParams.query['bool']['should'] = [];
-            delete queryParams.query['bool']["minimum_should_match"];
-
-            var keyName = fields[oSettings.aaSorting[0][0]]
-
-            var sortingJson = {};
-            sortingJson[keyName['mData']] = { "order": oSettings.aaSorting[0][1] };
-            queryParams.sort = [sortingJson];
-
-            queryParams['size'] = oSettings._iDisplayLength;
-            queryParams['from'] = oSettings._iDisplayStart;
-
-            // queryParams.query['bool']['must'].push({ "match": { "acc_id":SESSION_OBJ.orgs[0]  } });
-
-            var searchText = oSettings.oPreviousSearch.sSearch.trim();
-
-            if (searchText) {
-                queryParams.query['bool']['should'].push({ "wildcard": { "sname": "*" + searchText + "*" } });
-                queryParams.query['bool']['should'].push({ "wildcard": { "sname": "*" + searchText.toLowerCase() + "*" } });
-                queryParams.query['bool']['should'].push({ "wildcard": { "sname": "*" + searchText.toUpperCase() + "*" } });
-                queryParams.query['bool']['should'].push({ "wildcard": { "sname": "*" + capitalizeFLetter(searchText) + "*" } })
-                queryParams.query['bool']["minimum_should_match"] = 1;
-                queryParams.query['bool']['should'].push({
-                    "match_phrase": {
-                        "sname.keyword": "*" + searchText + "*"
-                    }
-                })
-                queryParams.query['bool']['should'].push({
-                    "match_phrase_prefix": {
-                        "sname.keyword": {
-                            "query": "*" + searchText + "*"
-                        }
-                    }
-                });
-            }
-
-            oSettings.jqXHR = $.ajax({
-                "dataType": 'json',
-                "contentType": 'application/json',
-                "type": "POST",
-                "url": sSource,
-                "data": JSON.stringify({"query":queryParams}),
-                success: function (data) {
-
-                    var resultData = data.result.data;
-
-                    student_list = resultData.data;
-
-                    $(".totalCount").html(data.result.total)
-
-                    resultData['draw'] = oSettings.iDraw;
-                    fnCallback(resultData);
-                }
-            });
-        },
-        "initComplete": function (settings, json) {
+        ],
+        dom:'1<"toolbar">frtip',
+        initComplete : function(){
+            $("div.toolbar").html('<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-plus-square" style="color: black;"aria-hidden="true"></i>Add Devices</button><button type="button" class="btn btn-secondary" data-toggle="modal"><i class="fa fa-refresh" style="color: black;" aria-hidden="true"></i></button>');
         }
-    };
+    } );
+} );
 
-    StudentTable = $("#student_table").DataTable(tableOption);
-}
+
 
