5dbe01930691fe0dec63e66af619193cc3aea15d
Rahika <rathi2000ka@gmail.com>
2021-01-07 11:13:12 +0530

    pull

--- webapps/js/managetanks.js
+++ webapps/js/managetanks.js
@@ -199,8 +199,8 @@ function tankDetails() {
 }
 $(function() {
 
-    var start = moment().subtract(29, 'days');
-    var end = moment();
+    // var start = moment().subtract(29, 'days');
+    // var end = moment();
 
     function cb(start, end) {
         $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
